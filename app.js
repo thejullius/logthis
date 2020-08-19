d0dac9aa044c1bf08bef31705447ef26aa1d76b2
Julio Goncalves <julio-goncalves@outlook.com>
2020-08-19 15:31:25 -0300

    chore: add config

--- app.js
+++ app.js
@@ -1,122 +1,129 @@
-const fetch = require('node-fetch')
-const moment = require('moment')
-const Pretty = require('pretty-error')
-const queryString = require('query-string')
-require('dotenv').config()
+const fetch = require("node-fetch");
+const moment = require("moment");
+const Pretty = require("pretty-error");
+const queryString = require("query-string");
 
 const {
   SLACK_TOKEN: token,
   SLACK_LOG_CHANNEL: channel,
   SLACK_ICON_URL: icon_url,
-  SLACK_USERNAME: username
-} = process.env
+  SLACK_USERNAME: username,
+} = process.env;
 
 const params = {
   token,
   channel,
   icon_url,
-  username
-}
+  username,
+};
 
-const url = 'https://slack.com/api'
-const headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+const url = "https://slack.com/api";
+const headers = { "Content-Type": "application/x-www-form-urlencoded" };
 
-async function postMessage (message) {
+async function postMessage(message) {
   try {
     const options = {
-      method: 'post',
-      headers
-    }
+      method: "post",
+      headers,
+    };
 
-    const query = queryString.stringify({ ...params, ...message })
-    await fetch(`${url}/chat.postMessage?${query}`, options)
+    const query = queryString.stringify({ ...params, ...message });
+    await fetch(`${url}/chat.postMessage?${query}`, options);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-async function getHistory (payload) {
+async function getHistory(payload) {
   const options = {
-    method: 'get',
-    headers
-  }
+    method: "get",
+    headers,
+  };
 
-  const query = queryString.stringify({ ...params, ...payload })
-  return await fetch(`${url}/conversations.history?${query}`, options)
+  const query = queryString.stringify({ ...params, ...payload });
+  return await fetch(`${url}/conversations.history?${query}`, options);
 }
 
-function sendNotify ({ file, resume, details }) {
-  const text = JSON.stringify(details, null, '  ')
+function sendNotify({ file, resume, details }) {
+  const text = JSON.stringify(details, null, "  ");
   const attachments = [
     {
-      fallback: 'Error while rendering message',
+      fallback: "Error while rendering message",
       pretext: `*${file}*`,
-      title: resume.replace(/  +/g, ''),
-      color: '#3498db',
-      text: `${'```'}${text}${'```'}`,
-      ts: moment().format('X')
-    }
-  ]
+      title: resume.replace(/  +/g, ""),
+      color: "#3498db",
+      text: `${"```"}${text}${"```"}`,
+      ts: moment().format("X"),
+    },
+  ];
 
-  postMessage({ attachments: JSON.stringify(attachments) })
-  console.log({ file, resume, details })
+  postMessage({ attachments: JSON.stringify(attachments) });
+  console.log({ file, resume, details });
 }
 
-async function sendError ({ file, payload, error }) {
-  const formattedError = new Pretty().withoutColors().render(error)
+async function sendError({ file, payload, error }) {
+  const formattedError = new Pretty().withoutColors().render(error);
 
   const blocks = [
     {
-      type: 'section',
+      type: "section",
       text: {
-        type: 'mrkdwn',
-        text: `*${file}*`
-      }
-    }
-  ]
+        type: "mrkdwn",
+        text: `*${file}*`,
+      },
+    },
+  ];
 
   const attachments = [
     {
-      fallback: 'Error while rendering message',
-      pretext: '*Error*',
-      color: '#e74c3c',
-      text: `${'```'}${formattedError}${'```'}`,
-      ts: moment().format('X')
-    }
-  ]
+      fallback: "Error while rendering message",
+      pretext: "*Error*",
+      color: "#e74c3c",
+      text: `${"```"}${formattedError}${"```"}`,
+      ts: moment().format("X"),
+    },
+  ];
 
   if (payload) {
     attachments.push({
-      fallback: 'Error while rendering message',
-      pretext: '*Payload*',
-      color: '#023e7d',
-      text: `${'```'}${JSON.stringify(payload, null, 2)}${'```'}`
-    })
+      fallback: "Error while rendering message",
+      pretext: "*Payload*",
+      color: "#023e7d",
+      text: `${"```"}${JSON.stringify(payload, null, 2)}${"```"}`,
+    });
   }
 
   const options = {
     text: `New error in ${file}`,
     attachments: JSON.stringify(attachments),
-    blocks: JSON.stringify(blocks)
-  }
+    blocks: JSON.stringify(blocks),
+  };
 
   const response = await getHistory({
-    oldest: moment().startOf('day').format('X'),
-    latest: moment().endOf('day').format('X')
-  })
+    oldest: moment().startOf("day").format("X"),
+    latest: moment().endOf("day").format("X"),
+  });
 
-  const { messages } = await response.json()
+  const { messages } = await response.json();
 
   for (const message of messages) {
     if (message.text === options.text) {
-      options.thread_ts = message.ts
-      break
+      options.thread_ts = message.ts;
+      break;
     }
   }
 
-  postMessage(options)
+  postMessage(options);
 
-  console.log(new Pretty().render(error))
+  console.log(new Pretty().render(error));
 }
 
-module.exports = { sendError, sendNotify }
+function configure(options) {
+  for (const key in options) {
+    if (options.hasOwnProperty(key)) {
+      params[key] = options[key];
+    }
+  }
+}
+
+module.exports = { sendError, sendNotify, configure };
